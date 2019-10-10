b8f528b97da33fb311c76a2f2afd51a8291c2c88
A.A.Isa <aabdulmajeed.isa@gmail.com>
2019-10-10 21:32:43 +0100

    change logo folder and name

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Row from 'react-bootstrap/Row';
-import appLogo from '../images/wltt-logo.png';
+import wlttLogo from '../assets/wlttLogo.png';
 const Header = () => {
   return (
     <Row className='justify-content-center'>
       <div className='text-center mt-4'>
-        <img src={appLogo} alt='app-logo' width='150px' />
+        <img src={wlttLogo} alt='app-logo' width='150px' />
         <h2>Link-to-ChatText</h2>
         <p>
           An app that converts article link into sharable chat text with article
